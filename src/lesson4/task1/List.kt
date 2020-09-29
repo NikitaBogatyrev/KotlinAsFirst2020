d390a45e937f516533e31d1ddcbcac4981f6de00
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-09-29 12:58:08 +0300

    3 lesson 2

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -133,7 +133,18 @@ fun abs(v: List<Double>): Double {
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    var a = 0.0
+    var b = 0.0
+    var k = 0.0
+    if (list.isEmpty()) return 0.0
+    for(i in 0 until list.size) {
+        a += list[i]
+        k++
+    }
+    b = a / k
+    return b
+}
 
 /**
  * Средняя (3 балла)
