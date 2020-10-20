83eb41b636b26277986c040e1431a21c79b3c71f
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-10-21 00:39:01 +0300

    isCoPrime

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -175,7 +175,23 @@ fun lcm(m: Int, n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    var t = 0
+    var q = n
+    var w = m
+    if (q >= w) {
+        t = w
+        w = q
+        q = t
+    }
+    for (i in 2..w) {
+        if (q % i == 0 && w % i == 0) return false
+    }
+    return true
+}
+
+
+
 
 /**
  * Средняя (3 балла)
