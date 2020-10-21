073f8cdd097936dc88ba964784f60d1319add299
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-10-21 16:23:36 +0300

    ageDescription

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -72,7 +72,7 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
 fun ageDescription(age: Int): String {
     return when {
         (age % 10 == 1) && (age != 11 && age != 111) -> "$age год"
-        (age % 10 in 2..4) && (age % 100 !in 12..14) -> "$age года"
+        (age % 10 in 2..4) && (age % 100 !in 11..14) -> "$age года"
         else -> "$age лет"
     }
 }
