13544f0fe7065793a4f2652ca6c874ac38fcac28
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-10-23 17:34:54 +0300

    factorizeToString fix v2.0

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -244,7 +244,21 @@ fun factorize(n: Int): List<Int> {
  * Результат разложения вернуть в виде строки, например 75 -> 3*5*5
  * Множители в результирующей строке должны располагаться по возрастанию.
  */
-fun factorizeToString(n: Int): String = TODO()
+fun factorizeToString(n: Int): String {
+    var i = 2
+    var nn = n
+    var list = mutableListOf<Int>()
+    while (nn > 1) {
+        if (nn % i == 0) {
+            nn /= i
+            list.add(i)
+        } else i++
+
+    }
+    return list.joinToString(
+        separator = "*"
+    )
+}
 
 /**
  * Средняя (3 балла)
