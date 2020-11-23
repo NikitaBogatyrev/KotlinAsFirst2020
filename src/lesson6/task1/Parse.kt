dada0bdb3c16838a977de308ab535b9830021619
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-11-23 12:40:08 +0300

    dateDigitToStr

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -107,17 +107,15 @@ fun dateDigitToStr(digital: String): String {
         val a = month - 1
         val month1 = list[a]
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            if ((month == 2 && day <= 29) || ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30)
-                || (((month == 1 || (month == 3) || (month == 5) || (month == 7) || (month == 8) ||
-                        (month == 10) || (month == 12)) && day <= 31))
-            ) return String.format("%d %s %d", day, month1, year)
+            if (month == 2 && day <= 29) return String.format("%d %s %d", day, month1, year)
         }
         if ((year % 4 != 0) || (year % 100 == 0 && year % 400 != 0)) {
-            if ((month == 2 && day <= 28) || ((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30)
-                || (((month == 1 || (month == 3) || (month == 5) || (month == 7) || (month == 8)
-                        || (month == 10) || (month == 12)) && day <= 31))
-            ) return String.format("%d %s %d", day, month1, year)
+            if (month == 2 && day <= 28) return String.format("%d %s %d", day, month1, year)
         }
+        if (((month == 4 || month == 6 || month == 9 || month == 11) && day <= 30)
+            || (((month == 1 || (month == 3) || (month == 5) || (month == 7) || (month == 8) ||
+                    (month == 10) || (month == 12)) && day <= 31)))
+            return String.format("%d %s %d", day, month1, year)
     } catch (e: NumberFormatException) {
         return ""
     }
