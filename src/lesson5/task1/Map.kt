a2400490b0b7c449e623294d25ab4b6c2927a329
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-12-06 14:37:26 +0300

    whoAreInBoth

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -137,13 +137,14 @@ fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>): Map<Strin
  */
 fun whoAreInBoth(a: List<String>, b: List<String>): List<String> {
     val c = mutableSetOf<String>()
+    val ccc = listOf("")
     for (i in a.indices) {
         for (i in b.indices)
             if (a[i] == b[i]) c.add(a[i])
     }
     val cc = mutableListOf<String>()
     for (item in c) {
-        if (item.isEmpty()) return cc
+        if (item.isEmpty()) return ccc
         cc.add(item)
     }
     return cc
