3f91b10cde97b69cd218b8245d5ddd45b5a9daf7
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-12-07 16:02:56 +0300

    daysInMonth

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -36,7 +36,18 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        if (month == 2) return 29
+    }
+    if ((year % 4 != 0) || (year % 100 == 0 && year % 400 != 0)) {
+        if (month == 2) return 28
+    }
+    if (month == 4 || month == 6 || month == 9 || month == 11) return 30
+    if (month == 1 || month == 3 || month == 5 || month == 7
+        || month == 8 || month == 10 || month == 12) return 31
+    return -1
+}
 
 /**
  * Простая (2 балла)
