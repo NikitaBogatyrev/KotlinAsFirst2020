c47d89405198c4429ee71a8c52a72d80b6448f23
NikitaBogatyrev <nik.bogatyrev.2001@gmail.com>
2020-12-20 10:10:36 +0300

    plusMinus

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -211,7 +211,7 @@ fun plusMinus(expression: String): Int {
     var sum = 0
     var sumMinus = 0
     for (i in jump.indices) {
-        if ((jump.size == 2) || (jump[i].contains(Regex("""(\+ | \-)\s(\+ | \-)"""))) ||
+        if ((jump.size == 2) || (jump[i].contains(Regex("""\s | (\s(\+\-\s))"""))) ||
             (jump[i].contains(Regex("""\-\d"""))) ||
             (jump[i].contains(Regex("""\+\d"""))) ||
             (jump[0] == "+" || jump[0] == "-") ||
